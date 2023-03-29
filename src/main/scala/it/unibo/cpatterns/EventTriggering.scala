cc825e30546bdcb6b70354c44cdf4e864c0b5404
Roberto Casadei <robyonrails@gmail.com>
2023-03-29 14:51:30 +0200

    Fix termination

--- src/main/scala/it/unibo/cpatterns/EventTriggering.scala
+++ src/main/scala/it/unibo/cpatterns/EventTriggering.scala
@@ -12,6 +12,8 @@ class EventTriggering extends SimulatedAggregateProgram {
   val BUBBLE_RADIUS = 100
 
   val BUBBLE_DURATION_ROUNDS = 15
+  val HANDLING_BUBBLE_DURATION_ROUNDS = 30
+
 
   override def main(): Any = {
     node.put("event-detector", eventDetector)
@@ -21,7 +23,7 @@ class EventTriggering extends SimulatedAggregateProgram {
     case class Event(detector: ID)(val handler: Option[ID] = None)
 
     def detectEvents(): Set[Event] = {
-      var value = rep(scala.util.Random.nextInt(100))(_+1) % 100
+      val value = rep(scala.util.Random.nextInt(100))(_ + 1) % 100
       node.put("value", value)
       if(value == 0) {
         //println(s"${mid()} got an event!")
@@ -35,7 +37,9 @@ class EventTriggering extends SimulatedAggregateProgram {
       val t = broadcast(source, if(source) ownT else 0)
       val g = distanceTo(source)
       node.put(s"${e}_g", g)
-      val status = if(g < BUBBLE_RADIUS && t < BUBBLE_DURATION_ROUNDS) Output else External
+      val status = if(source && t >= HANDLING_BUBBLE_DURATION_ROUNDS) Terminated
+        else if(g < BUBBLE_RADIUS && t < HANDLING_BUBBLE_DURATION_ROUNDS) Output
+        else External
       POut((), status)
     }
 
@@ -50,20 +54,25 @@ class EventTriggering extends SimulatedAggregateProgram {
         val decidedHandler = rep(NO_HANDLER)(h => {
           if(h != NO_HANDLER) h else handlers.maxOption.getOrElse(NO_HANDLER)
         })
-        if(decidedHandler != NO_HANDLER  && leader) { println(s"${mid} chose handler ${decidedHandler}") }
+        if(decidedHandler != NO_HANDLER  && leader) { println(s"${mid()} chose handler ${decidedHandler}") }
         val chosenHandler = broadcast(leader, decidedHandler)
         val eventToHandle = broadcast(leader, event)
         val ownT = rep(0)(_ + (if(chosenHandler != NO_HANDLER) 1 else 0))
         val t = broadcast(leader, if (leader) ownT else 0)
-        POut(eventToHandle, if(withinBubble && t < BUBBLE_DURATION_ROUNDS) {
+        POut(eventToHandle, /*if(leader && t==0 && events.isEmpty) External
+        else*/ if(withinBubble && t <= BUBBLE_DURATION_ROUNDS) {
           node.put("reporting", 1)
           if(mid() == chosenHandler) { /* println(s"${mid} was chosen for events ${eventToHandle}!"); */ Output } else Bubble
-        } else External)
+        } else if(withinBubble && t > BUBBLE_DURATION_ROUNDS) Terminated else  External)
       }, events, {}).values.toSet
     }
 
-    val detectedEvents: Set[Event] = branch(eventDetector){ detectEvents() } { Set.empty }
+    val detectedEvents: Set[Event] = mux(eventDetector){ detectEvents() } { Set.empty }
+    node.put("events_detected", detectedEvents)
+    node.put("events_detected_any", !detectedEvents.isEmpty)
     val eventsToHandle: Set[Event] = reportEvents(detectedEvents).map(_.copy()(handler = Some(mid())))
+    node.put("events_to_handle", eventsToHandle)
+    node.put("events_to_handle_any", !eventsToHandle.isEmpty)
     // if(!eventsToHandle.isEmpty) { println(s"${mid} has to handle ${eventsToHandle}") }
 
     val map = sspawn[Event, Unit, Unit](pid => args => eventHandlerProcess(pid)(args),
--- src/main/scala/it/unibo/cpatterns/SimulationSupport.scala
+++ src/main/scala/it/unibo/cpatterns/SimulationSupport.scala
@@ -14,10 +14,35 @@ trait SimulatedAggregateProgram extends AggregateProgram
   def broadcastAlong[T](g: Double, v: T): T =
     G_along[T](g, nbrRange, v, x => x)
 
+
+  override def sspawn[K, A, R](process: K => A => POut[R], params: Set[K], args: A): Map[K,R] =
+    spawn2[K,A,Option[R]](k => a => handleOutput(handleTermination(process(k)(a))), params, args)
+      .collectValues { case Some(p) => p }
+
+  import SpawnInterface._
+
   override def runOnSharedKeysWithShare[K, A, R](process: K => (R, Boolean), params: Set[K]): Map[K, R] =
     share(Map.empty[K, R])((loc, nbr) => {
       (includingSelf.unionHoodSet(nbr().keySet ++ params))
         .mapToValues(x => exportConditionally(process.apply(x)))
         .collectValues[R] { case (r, true) => r }
     })
+
+  /*
+  BEWARE: the interplay between asynchronicity and retention may cause termination failure.
+  In those case, it might be better to use a kind of memory of process keys to avoid re-entrance,
+  with a periodic garbage collection.
+  %
+  This version works with:
+    - RandomDiracComb(max=1.0, min=1.5), retention = 2.0
+    - ExponentialTime(1.0), retention= 2.0 | 5.0
+ */
+  override def handleTermination[T](out: POut[T]): POut[T] = {
+    share[(Boolean,Int,POut[T])]((false,0,out)){
+      case (loc,nbrd) =>
+        val mustTerminate = includingSelf.anyHood(nbrd()._1) || out.status==Terminated
+        val mustExit = includingSelf.everyHood(nbr { mustTerminate }) // || (mustTerminate && loc._2 == 0) // includingSelf.everyHood(nbr{mustTerminate})
+        (mustTerminate, 1, if(mustExit) POut(out.result, External) else if(mustTerminate) POut(out.result, Terminated) else out)
+    }._3
+  }
 }
