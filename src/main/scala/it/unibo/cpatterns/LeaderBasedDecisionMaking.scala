c2fc3cea702086421bd75fd17ae121b2b82bd5e6
Roberto Casadei <roby.casadei@unibo.it>
2024-03-21 12:22:25 +0100

    skeleton for leader-based decision making

---
+++ src/main/scala/it/unibo/cpatterns/LeaderBasedDecisionMaking.scala
@@ -0,0 +1,16 @@
+package it.unibo.cpatterns
+
+import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
+
+class LeaderBasedDecisionMaking extends SimulatedAggregateProgram with BlockT {
+  import it.unibo.cpatterns.LeaderBasedDecisionMaking._
+
+  override def main(): Any = {
+
+  }
+
+}
+
+object LeaderBasedDecisionMaking {
+
+}
